37497f22c4010753e66072225a6a59fa7757909d
ThanhLoc <thanhloclol6@gmail.com>
2023-08-11 12:50:50 +0700

    finish post + user router and get + add comment

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
-const User = require("../models/User");
 
 //CREATE A POST
 router.post("/", async (req, res) => {
@@ -19,16 +18,16 @@ router.put("/:id", async (req, res) => {
     const post = await Post.findById(req.params.id.trim());
     if (post.userId === req.body.userId) {
       await post.updateOne({ $set: req.body });
-      res.status(200).json("Post has been updated");
+      return res.status(200).json("Post has been updated");
     } else {
-      res.status(403).json("You can only update your post");
+      return res.status(403).json("You can only update your post");
     }
   } catch (err) {
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 //DELETE A POST
-router.get("/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (req.body.userId === post.userId) {
       try {
@@ -38,17 +37,87 @@ router.get("/:id", async (req, res) => {
         res.status(500).json(err);
       }
     } else {
-      res.send(403).json("You can't delete other people post");
+      res.sendStatus(403).json("You can't delete other people post");
     }
   });
 //GET ALL POST FROM A USER
 router.get("/user/:id", async (req, res) => {
-    try {
-      const post = await Post.find({ userId: req.params.id });
-      res.status(200).json(post);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  try {
+    const post = await Post.find({ userId: req.params.id });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//GET ALL POSTS
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+//UPVOTE A POST
+router.put("/upvote/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id.trim());
+    if (!post.upvotes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { upvotes: req.body.userId } });
+      res.status(200).json("Post is upvoted!");
+    } else if (post.upvotes.includes(req.body.userId)) {
+      await post.updateOne({ $pull: { upvotes: req.body.userId } });
+      res.status(200).json("Post is no longer upvoted!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DOWNVOTE A POST
+router.put("/downvote/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id.trim());
+    if (!post.downvotes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { downvotes: req.body.userId } });
+      res.status(200).json("Post is downvoted!");
+    } else if (post.downvotes.includes(req.body.userId)) {
+      await post.updateOne({ $pull: { downvotes: req.body.userId } });
+      res.status(200).json("Post is no longer downvoted!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//ADD A COMMENT
+router.post("/comment/:id", async (req, res) => {
+  try {
+    await Post.findByIdAndUpdate(req.params.id, {
+      $push: {
+        comments: { content: req.body.content, ownerId: req.body.ownerId },
+      },
+    });
+    res.status(200).json("A comment is added");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL COMMENTS IN A POST
+router.get("/comment/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    const comments = post.comments;
+    const commentArr = await fetchComments(comments);
+    console.log(commentArr);
+    res.status(200).json(commentArr);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE COMMENT IN A POST
+
+//EDIT COMMENT IN A POST
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -23,5 +23,28 @@ router.put("/:id", async (req, res) => {
       res.status(500).json(err);
     }
   });
+//DELETE A USER
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can only delete your account");
+  }
+});
 
+//GET A USER
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   module.exports = router;
