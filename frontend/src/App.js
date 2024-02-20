55bdf38e17fe9b5a659253f0f9bc9ce5dcdf47e2
ThanhLoc <thanhloclol6@gmail.com>
2024-02-20 19:36:59 +0700

    add vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserProfile from "./Components/UserProfile/UserProfile";
@@ -8,7 +7,6 @@ import Register from "./Components/Auth/Register";
 import HomePage from "./Components/Feed/HomePage/HomePage";
 import RequireAuth from "./Components/RequireAuth/RequireAuth";
 import News from "./Components/Feed/News/News";
-import Footer from "./Components/Footer/Footer";
 import Friends from "./Components/Friends/Friends";
 import ChatRoom from "./Components/ChatOverview/ChatRoom";
 
